2aed5c8de180b75021bc8d402501c8d3affaff97
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-12 20:57:35 +0330

    validation

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ mongoose
 
 //* create a schema
 const userSchema = new mongoose.Schema({
-  first_name: String,
-  last_name: { type: String, required: true },
-  favourites: [String],
+  first_name: { type: String, minlength: 3, maxlength: 20 },
+  last_name: {
+    type: String,
+    required: function () {
+      return this.admin;
+    },
+  },
+  favourites: { type: [String], enum: ["sport", "politic", "music"] },
   date: { type: Date, default: Date.now },
   admin: Boolean,
+  age: { type: Number, min: 8, max: 120 },
 });
 
 //* create a model
@@ -26,9 +32,13 @@ const createUser = async () => {
     admin: true,
   });
 
-  //? save user in database
-  const result = await user.save();
-  console.log(result);
+  try {
+    //? save user in database
+    const result = await user.save();
+    console.log(result);
+  } catch (ex) {
+    console.log(ex.message);
+  }
 };
 
 // createUser();
@@ -43,20 +53,22 @@ async function getUser() {
 }
 
 async function updateUser(id) {
-  const user = await User.update({_id:id},{
-    $set:{
-      first_name:"updated name 2"
+  const user = await User.update(
+    { _id: id },
+    {
+      $set: {
+        first_name: "updated name 2",
+      },
     }
-  })
+  );
   console.log(user);
 }
 
-async function removeUser(id){
-  const result= await User.deleteOne({_id:id});
+async function removeUser(id) {
+  const result = await User.deleteOne({ _id: id });
   console.log(result);
 }
 
 // getUser();
 // updateUser("63bfff8e3120272aba9f74dc");
-removeUser("63bfff8e3120272aba9f74dc");
-
+// removeUser("63bfff8e3120272aba9f74dc");
