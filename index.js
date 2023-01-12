aef9467b9b0b1fd48bed25fcf81498ba36a41d42
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-12 15:55:29 +0330

    create a schema

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/mongoproject")
   .then(() => console.log("connected to mongodb"))
   .catch(() => console.log("could not connect to mongodb"));
+
+const userSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: { type: String, required: true },
+  favourites: [String],
+  date: { type: Date, default: Date.now },
+  admin: Boolean,
+});
